21524088ee6a8ef7053c1391130cab5c04a6abbd
Anderson <anderson.ti@plasutil.com.br>
2020-04-16 20:09:20 -0300

    Primeiro commit e funcionando

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste com FIREBASE</title>
+</head>
+<body>
+
+    <div id="token"></div>
+
+
+
+    <!-- The core Firebase JS SDK is always required and must be listed first -->
+    <script defer src="https://www.gstatic.com/firebasejs/7.14.0/firebase-app.js"></script>
+    <script defer src="https://www.gstatic.com/firebasejs/7.14.0/firebase-auth.js"></script>
+    <script defer src="https://www.gstatic.com/firebasejs/7.14.0/firebase-firestore.js"></script>
+    <script defer src="https://www.gstatic.com/firebasejs/7.14.0/firebase-messaging.js"></script>
+
+    <!-- TODO: Add SDKs for Firebase products that you want to use
+        https://firebase.google.com/docs/web/setup#available-libraries -->
+    <script defer src="https://www.gstatic.com/firebasejs/7.14.0/firebase-analytics.js"></script>
+
+    <script defer src="app.js"></script>
+</body>
+</html>
